f0c13eb25f68c1e5c8b3f900e01a1794c55aaff4
princetondcunha <princeton_dcunha@outlook.com>
2023-10-11 07:47:47 -0300

    Setup Test Hello for Discord Bot

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,30 @@
 '''Discord Bot'''
 
+import os
 import discord
 from discord.ext import commands
+import dotenv
+
+dotenv.load_dotenv()
+discord_token = os.environ.get('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
 
 # Create a bot instance
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!',intents=intents)
+intents.message_content = True
 
 # Bot event: When the bot is ready
 @bot.event
 async def on_ready():
+    '''On Ready'''
     print(f'Logged in as {bot.user.name}')
 
 # Bot command: A simple command that replies to a specific message
 @bot.command()
 async def hello(ctx):
+    '''Hello'''
     await ctx.send('Hello, world!')
 
 # Run the bot using your token
-bot.run("YOUR_BOT_TOKEN")
+bot.run(discord_token)
