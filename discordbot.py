87741c4ba5be3ccd376847d31b306bc7c90f3c6a
princetondcunha <princeton_dcunha@outlook.com>
2023-10-11 15:58:06 -0300

    Update Discord Bot to make REST calls

--- discordbot.py
+++ discordbot.py
@@ -1,12 +1,23 @@
 '''Discord Bot'''
 
 import os
+from urllib import request
+from wsgiref import headers
+import json
 import discord
 from discord.ext import commands
 import dotenv
+import requests
 
 dotenv.load_dotenv()
 discord_token = os.environ.get('DISCORD_TOKEN')
+staff_list_value = os.environ.get('STAFF_LIST')
+serviceid_value = os.environ.get('SERVICEID')
+user_identity_value = os.environ.get('USER_IDENTITY')
+
+URL = "https://outlook.office365.com/owa/calendar/"
+URL+= user_identity_value
+URL+= "/bookings/service.svc/GetStaffBookability"
 
 intents = discord.Intents.default()
 
@@ -25,6 +36,23 @@ async def on_ready():
 async def hello(ctx):
     '''Hello'''
     await ctx.send('Hello, world!')
+
+@bot.command()
+async def check_bookings(ctx):
+    '''Check Microsoft Bookings'''
+    payload = json.dumps({
+    "StaffList": [staff_list_value],
+    "Start": "2023-01-01T00:00:00",
+    "End": "2025-01-01T00:00:00",
+    "TimeZone": "America/Halifax",
+    "ServiceId": serviceid_value
+    })
+    headers = {
+        "Content-Type":"application/json"
+    }
+    response = requests.request(
+    "POST", URL, headers=headers, data=payload, timeout=10)
+    print(response.text)
 
 # Run the bot using your token
 bot.run(discord_token)
