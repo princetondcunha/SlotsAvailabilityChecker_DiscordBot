f72ffd3ca96dd8bab4d3ff2c7afdf94914b18ebd
princetondcunha <princeton_dcunha@outlook.com>
2023-10-11 00:20:47 -0300

    Reformated URL variable initialization

--- main.py
+++ main.py
@@ -10,27 +10,31 @@ staff_list_value = os.environ.get('STAFF_LIST')
 serviceid_value = os.environ.get('SERVICEID')
 user_identity_value = os.environ.get('USER_IDENTITY')
 
-URL = f"https://outlook.office365.com/owa/calendar/{user_identity_value}/bookings/service.svc/GetStaffBookability"
+URL = "https://outlook.office365.com/owa/calendar/"
+URL+= user_identity_value
+URL+= "/bookings/service.svc/GetStaffBookability"
 
 payload = json.dumps({
-  "StaffList": [staff_list_value],
-  "Start": "2023-10-07T00:00:00",
-  "End": "2023-11-02T00:00:00",
-  "TimeZone": "America/Halifax",
-  "ServiceId": serviceid_value
+    "StaffList": [staff_list_value],
+    "Start": "2023-01-01T00:00:00",
+    "End": "2025-01-01T00:00:00",
+    "TimeZone": "America/Halifax",
+    "ServiceId": serviceid_value
 })
 
 headers = {
-  'Content-Type': 'application/json'
+    'Content-Type': 'application/json'
 }
 
-response = requests.request("POST", URL, headers=headers, data=payload, timeout=10)
+response = requests.request(
+    "POST", URL, headers=headers, data=payload, timeout=10)
 
-slots = json.loads(response.text)["StaffBookabilities"][0]["BookableTimeBlocks"]
+slots = json.loads(response.text)[
+    "StaffBookabilities"][0]["BookableTimeBlocks"]
 
-slots_count=len(slots)
+slots_count = len(slots)
 
-if slots_count>0:
-    print("Slots Available :",slots_count)
+if slots_count > 0:
+    print("Slots Available :", slots_count)
 else:
     print("No slots available")
