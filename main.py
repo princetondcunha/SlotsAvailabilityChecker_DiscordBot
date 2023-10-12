e52266f6a03c6592354efe563f44b88d01fe4e0a
princetondcunha <princeton_dcunha@outlook.com>
2023-10-12 09:57:35 -0300

    Re-arrange everything at the project directory level

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
-'''Slots Availability Checker - Python'''
+'''Discord Bot'''
 
-import json
 import os
-import requests
+import json
 import dotenv
+import discord
+from discord.ext import commands
+from request import restrequest
 
 dotenv.load_dotenv()
-staff_list_value = os.environ.get('STAFF_LIST')
-serviceid_value = os.environ.get('SERVICEID')
-user_identity_value = os.environ.get('USER_IDENTITY')
+discord_token = os.environ.get('DISCORD_TOKEN')
 
-URL = "https://outlook.office365.com/owa/calendar/"
-URL+= user_identity_value
-URL+= "/bookings/service.svc/GetStaffBookability"
+intents = discord.Intents.default()
 
-payload = json.dumps({
-    "StaffList": [staff_list_value],
-    "Start": "2023-01-01T00:00:00",
-    "End": "2025-01-01T00:00:00",
-    "TimeZone": "America/Halifax",
-    "ServiceId": serviceid_value
-})
+# Create a bot instance
+bot = commands.Bot(command_prefix='!',intents=intents)
+intents.message_content = True
 
-headers = {
-    'Content-Type': 'application/json'
-}
+# Bot event: When the bot is ready
+@bot.event
+async def on_ready():
+    '''On Ready'''
+    print(f'Logged in as {bot.user.name}')
 
-response = requests.request(
-    "POST", URL, headers=headers, data=payload, timeout=10)
+# Bot command: A simple command that replies to a specific message
+@bot.command()
+async def hello(ctx):
+    '''Hello'''
+    await ctx.send('Hello, world!')
 
-slots = json.loads(response.text)[
-    "StaffBookabilities"][0]["BookableTimeBlocks"]
+@bot.command()
+async def check_bookings(ctx):
+    '''Check Microsoft Bookings'''
+    response = restrequest()
+    data = json.loads(response.text)
 
-slots_count = len(slots)
+    if 'items' in data:
+        await ctx.send("Slots available")
+    else:
+        await ctx.send("No slots")
 
-if slots_count > 0:
-    print("Slots Available :", slots_count)
-else:
-    print("No slots available")
+# Run the bot using your token
+bot.run(discord_token)
